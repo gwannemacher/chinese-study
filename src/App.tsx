12495e33bbe9c60a185392e8014760eb8cd9ec8f
Grace Wannemacher <gwannemacher@gmail.com>
2023-01-10 19:03:23 -0500

    lesson-17: get basic flashcard functionality working for chapter 17

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
 import { useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
+import { Lessons } from "./data/lessons";
 
-function App() {
-  return <Card />;
+export enum ToggleState {
+  FRONT,
+  BACK,
 }
+
+const getRandom = (listLength: number): number =>
+  Math.floor(Math.random() * (listLength - 1));
+
+const App = () => {
+  const lessons = Lessons;
+  const lesson = lessons[0];
+  const [randomIndex, setRandomIndex] = useState(
+    getRandom(lesson.characters.length)
+  );
+  const [toggleState, setToggleState] = useState(ToggleState.FRONT);
+
+  return (
+    <div style={{ display: "flex", gap: ".5em" }}>
+      <Card
+        chapter={lesson.chapter}
+        character={lesson.characters[randomIndex]}
+        toggleState={toggleState}
+        setToggleState={setToggleState}
+      />
+      <button
+        onClick={() => {
+          setToggleState(ToggleState.FRONT);
+          setRandomIndex(getRandom(lesson.characters.length));
+        }}
+      >
+        next
+      </button>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/data/lesson17.ts
@@ -0,0 +1,30 @@
+import { Lesson } from "../types/lesson";
+
+export const Lesson17: Lesson = {
+    chapter: 17, 
+    characters: [
+        { hanzi: '逛', pinyin: 'guàng', definition: 'wander' },
+        { hanzi: '裙子', pinyin: 'qúnzi', definition: 'skirt' },
+        { hanzi: '顶', pinyin: 'dǐng', definition: '(a measure word for hats)' },
+        { hanzi: '帽子', pinyin: 'màozi', definition: 'hat' },
+        { hanzi: '这么', pinyin: 'zhème', definition: 'so' },
+        { hanzi: '陪', pinyin: 'péi', definition: 'to accompany' },
+        { hanzi: '风景', pinyin: 'fēngjǐng', definition: 'scenery' },
+        { hanzi: '特别', pinyin: 'tèbié', definition: 'especially' },
+        { hanzi: '美', pinyin: 'měi', definition: 'beautiful' },
+        { hanzi: '烤鸭', pinyin: 'kǎoyā', definition: 'roast duck' },
+        { hanzi: '只', pinyin: 'zhǐ', definition: '(a measure word for animals)' },
+        { hanzi: '病', pinyin: 'bìng', definition: 'to be ill; disease; illness' },
+        { hanzi: '抽(时间)', pinyin: 'chōu (shíjiān)', definition: 'to find (time to do)' },
+        { hanzi: '时间', pinyin: 'shíjiān', definition: 'time' },
+        { hanzi: '看', pinyin: 'kàn', definition: 'to call on, to visit, to see' },
+        { hanzi: '已经', pinyin: 'yǐjīng', definition: 'already' },
+        { hanzi: '以前', pinyin: 'yǐqián', definition: 'before' },
+        { hanzi: '上', pinyin: 'shàng', definition: 'last' },
+        { hanzi: '洗', pinyin: 'xǐ', definition: 'to wash' },
+        { hanzi: '衣服', pinyin: 'yīfú', definition: 'clothes' },
+        { hanzi: '枝', pinyin: 'zhī', definition: '(a measure word for flowers with stems intact)' },
+        { hanzi: '洗澡', pinyin: 'xǐzǎo', definition: 'to take a shower' },
+        { hanzi: '玩儿', pinyin: 'wán er', definition: 'to play' }
+    ]
+};
