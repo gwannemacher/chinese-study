c6aa50b6dd2386163a608d2004518b9e6ae11cc0
Grace Wannemacher <gwannemacher@gmail.com>
2023-01-28 18:57:51 -0500

    lesson4: add lesson 4

---
+++ src/data/lesson4.ts
@@ -0,0 +1,31 @@
+import { Lesson } from "../types/lesson";
+
+export const Lesson4: Lesson = {
+    chapter: 4, 
+    characters: [
+        { hanzi: '要', pinyin: 'yào', definition: 'to want' },
+        { hanzi: '换', pinyin: 'huàn', definition: 'to change' },
+        { hanzi: '钱', pinyin: 'qián', definition: 'money' },
+        { hanzi: '多少', pinyin: 'duōshao', definition: 'how much; how many' },
+        { hanzi: '一', pinyin: 'yī', definition: 'one' },
+        { hanzi: '百', pinyin: 'bǎi', definition: 'hundred' },
+        { hanzi: '美元', pinyin: 'měiyuán', definition: 'US dollar' },
+        { hanzi: '二', pinyin: 'èr', definition: 'two' },
+        { hanzi: '三', pinyin: 'sān', definition: 'three' },
+        { hanzi: '四', pinyin: 'sì', definition: 'four' },
+        { hanzi: '五', pinyin: 'wǔ', definition: 'five' },
+        { hanzi: '两', pinyin: 'liǎng', definition: 'two' },
+        { hanzi: '杯', pinyin: 'bēi', definition: 'cup; glass' },
+        { hanzi: '块(元)', pinyin: 'kuài(yuán)', definition: 'yuan (the basic monetary unit of China)' },
+        { hanzi: '个', pinyin: 'gè', definition: '(a measure word most extensively used)' },
+        { hanzi: '六', pinyin: 'liù', definition: 'six' },
+        { hanzi: '毛(角)', pinyin: 'máo(jiǎo)', definition: 'mao/jian (the fractional monetary unit of China, =1/10 of a yuan)' },
+        { hanzi: '瓶', pinyin: 'píng', definition: 'bottle' },
+        { hanzi: '七', pinyin: 'qī', definition: 'seven' },
+        { hanzi: '八', pinyin: 'bā', definition: 'eight' },
+        { hanzi: '本', pinyin: 'běn', definition: '(a measure word for books, etc.)' },
+        { hanzi: '九', pinyin: 'jiǔ', definition: 'nine' },
+        { hanzi: '十', pinyin: 'shí', definition: 'ten' },
+        { hanzi: '分', pinyin: 'fēn', definition: 'fen (the fractional monetary unit of China, =1/10 of a jiao)' },
+    ]
+};
