7b4eafdd3cc7151e210665f8bec53514bc970fc9
Grace Wannemacher <gwannemacher@gmail.com>
2023-01-28 18:43:05 -0500

    lesson5: add lesson 5

---
+++ src/data/lesson5.ts
@@ -0,0 +1,23 @@
+import { Lesson } from "../types/lesson";
+
+export const Lesson5: Lesson = {
+    chapter: 5, 
+    characters: [
+        { hanzi: '在', pinyin: 'zài', definition: 'to exist; to be at; in or on a place' },
+        { hanzi: '哪里', pinyin: 'nǎ li', definition: 'where' },
+        { hanzi: '就', pinyin: 'jiù', definition: 'just' },
+        { hanzi: '那里', pinyin: 'nà li', definition: 'there' },
+        { hanzi: '食堂', pinyin: 'shítáng', definition: 'dining hall' },
+        { hanzi: '留学生', pinyin: 'liúxuéshēng', definition: 'overseas student' },
+        { hanzi: '宿舍', pinyin: 'sùshè', definition: 'dormitory' },
+        { hanzi: '办公室', pinyin: 'bàngōngshì', definition: 'office' },
+        { hanzi: '楼', pinyin: 'lóu', definition: 'building' },
+        { hanzi: '邮局', pinyin: 'yóujú', definition: 'post office' },
+        { hanzi: '银行', pinyin: 'yínháng', definition: 'bank' },
+        { hanzi: '医院', pinyin: 'yīyuàn', definition: 'hospital' },
+        { hanzi: '商店', pinyin: 'shāngdiàn', definition: 'shop' },
+        { hanzi: '书店', pinyin: 'shūdiàn', definition: 'bookshop' },
+        { hanzi: '知道', pinyin: 'zhīdào', definition: 'to know' },
+        { hanzi: '去', pinyin: 'qù', definition: 'to go' },
+    ]
+};
