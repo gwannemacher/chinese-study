e7a06fa27e95fd4ea39470b296565696a0dadbdb
Grace Wannemacher <gwannemacher@gmail.com>
2023-02-02 22:12:32 -0500

    lesson3: add lesson 3

---
+++ src/data/lesson3.ts
@@ -0,0 +1,26 @@
+import { Lesson } from "../types/lesson";
+
+export const Lesson3: Lesson = {
+    chapter: 3, 
+    characters: [
+        { hanzi: '吃', pinyin: 'chī', definition: 'to eat' },
+        { hanzi: '什么', pinyin: 'shénme', definition: 'what' },
+        { hanzi: '饺子', pinyin: 'jiǎozi', definition: 'dumpling' },
+        { hanzi: '米饭', pinyin: 'mǐfàn', definition: 'cooked rice' },
+        { hanzi: '面条', pinyin: 'miàntiáo', definition: 'noodle' },
+        { hanzi: '面包', pinyin: 'miànbāo', definition: 'bread' },
+        { hanzi: '包子', pinyin: 'bāozi', definition: 'stuffed bun' },
+        { hanzi: '喝', pinyin: 'hē', definition: 'to drink' },
+        { hanzi: '啤酒', pinyin: 'píjiǔ', definition: 'beer' },
+        { hanzi: '茶', pinyin: 'chá', definition: 'tea' },
+        { hanzi: '咖啡', pinyin: 'kāfēi', definition: 'coffee' },
+        { hanzi: '矿泉水', pinyin: 'kuàngquánshuǐ', definition: 'mineral water' },
+        { hanzi: '牛奶', pinyin: 'niúnǎi', definition: 'milk' },
+        { hanzi: '买', pinyin: 'mǎi', definition: 'to buy' },
+        { hanzi: '词典', pinyin: 'cídiǎn', definition: 'dictionary' },
+        { hanzi: '本子', pinyin: 'běnzi', definition: 'notebook' },
+        { hanzi: '书', pinyin: 'shū', definition: 'book' },
+        { hanzi: '笔', pinyin: 'bǐ', definition: 'pen' },
+        { hanzi: '书包', pinyin: 'shūbāo', definition: 'schoolbag' }
+    ]
+};
